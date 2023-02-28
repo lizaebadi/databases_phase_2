e164566cd158667daf9f290967ebd4d44b05b356
lizaebadi <liza-1@hotmail.co.uk>
2023-02-28 21:32:18 +0000

    Test Driving a Repository Class Exercise

---
+++ app.rb
@@ -0,0 +1,17 @@
+require_relative 'lib/database_connection'
+require_relative 'lib/album_repository'
+
+DatabaseConnection.connect('music_library')
+
+result = DatabaseConnection.exec_params('SELECT * FROM albums;', [])
+
+result.each do |record|
+  p record
+end
+
+album_repository = AlbumRepository.new
+
+album_repository.all.each do |record|
+  p record
+end 
+
---
+++ spec/seeds_albums.sql
@@ -0,0 +1,7 @@
+TRUNCATE TABLE albums RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Doolittle', '1989', '1');
+INSERT INTO albums (title, release_year, artist_id) VALUES ('Surfer Rosa', '1988', '1');
