c522515280c3a4146057984ac74b4fb9f2cf4341
lizaebadi <liza-1@hotmail.co.uk>
2023-03-04 16:47:15 +0000

    designing a schema with two tables

---
+++ blog/posts_table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  title text,
+  content text
+);
+
+CREATE TABLE comments (
+  id SERIAL PRIMARY KEY,
+  content text,
+  name text,
+  post_id int,
+  constraint fk_post foreign key(post_id)
+    references posts(id)
+    on delete cascade
+);
